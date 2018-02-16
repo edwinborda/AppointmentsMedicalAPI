5b85d4331abc95a9a21fbe7ec35cf18a8e536167
Edwin Borda <edwin.borda@outlook.com>
2018-02-16 11:19:43 -0500

    Se realizan

---
+++ MedicalAppointmentsAPI/Validations/CreateAppointmentValidator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using FluenT;
+
+namespace MedicalAppointmentsAPI.Validations
+{
+    public class CreateAppointmentValidator : AbstractValidator<CreateAppointments>
+
+    {
+    }
+}
